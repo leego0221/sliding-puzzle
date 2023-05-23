0ea390c7efd6f2cecd6f58b4a56adf2138d86fcc
leego0221 <leego0221@naver.com>
2023-05-23 18:04:41 +0900

    슬라이딩 퍼즐 ver 1.2
    퍼즐 사이즈 변경 & 셔플 기능 추가

--- script.js
+++ script.js
@@ -1,19 +1,37 @@
-const block = document.querySelectorAll(".block");
+let size; // 블록의 가로 세로 크기
+let index, pointedNum, prevPointedNum;
 
-const TO_LEFT = -1;
-const TO_RIGHT = 1;
-const TO_UP = -4;
-const TO_DOWN = 4;
+const shuffleButton = document.getElementById("shuffle-btn");
 
-const START = 15;
-let pointedNum = START;
-let prevPointedNum = START;
-block[START].classList.add("pointed");
-block[START].classList.add("prev-pointed");
+function makePuzzle(num) {
+  const title = document.getElementById("title");
+  const border = document.getElementById("border");
+  title.innerText = `Sliding Puzzle! (${num}x${num})`;
+  size = num;
+  index = num * num - 1;
+
+  for (let i = 0; i < num * num; i++) {
+    const block = document.createElement("div");
+    block.className = "block";
+    block.id = `n${i + 1}`;
+    block.innerText = i + 1;
+    border.appendChild(block);
+
+    if (i === index) {
+      block.classList.add("pointed");
+      block.classList.add("prev-pointed");
+      block.innerText = "";
+    }
+  }
+
+  pointedNum = index;
+  prevPointedNum = index;
+  border.style.gridTemplateColumns = `repeat(${num}, 1fr)`;
+}
 
 function swapBlock(key) {
-  // 정해진 키 외의 값이 들어오면 버그 생김
-  if (key === TO_LEFT || key === TO_RIGHT || key === TO_UP || key === TO_DOWN) {
+  // 정해진 키(-1, 1, -size, size) 외의 값이 들어오면 버그 생김
+  if (Math.abs(key) === 1 || Math.abs(key) === size) {
     // 클래스 변경
     block[pointedNum].classList.remove("pointed");
     block[pointedNum + key].classList.add("pointed");
@@ -31,18 +49,18 @@ function swapBlock(key) {
 }
 
 function shuffleBlock() {
-  for (let i = 0; i < 200; i++) {
+  for (let i = 0; i < size * 50; i++) {
     let num = Math.floor(Math.random() * 4) + 10;
-    if (num === 10 && pointedNum % 4 !== 0) {
-      num = TO_LEFT;
-    } else if (num === 11 && pointedNum % 4 !== 3) {
-      num = TO_RIGHT;
-    } else if (num === 12 && pointedNum > 3) {
-      num = TO_UP;
-    } else if (num === 13 && pointedNum < 12) {
-      num = TO_DOWN;
+    if (num === 10 && pointedNum % size !== 0) {
+      num = -1;
+    } else if (num === 11 && pointedNum % size !== size - 1) {
+      num = 1;
+    } else if (num === 12 && pointedNum > size - 1) {
+      num = -size;
+    } else if (num === 13 && pointedNum < size * (size - 1)) {
+      num = size;
     } else {
-      num = 0; // 정해진 숫자 외 다른 것은 아무 일도 일어나지 않음
+      num = 0; // 0은 겉으로 아무 일도 일어나지 않음
     }
     swapBlock(num);
   }
@@ -61,23 +79,27 @@ function checkPuzzle() {
   });
 
   // 다 맞췄을 시 동작
-  if (check === 15) {
+  if (check === size * size - 1) {
     setTimeout(() => {
       alert("퍼즐을 다 맞췄습니다! 확인 버튼을 누르면 다시 섞입니다.");
       shuffleBlock();
-    }, 1000);
+    }, 100);
   }
 }
 
+function simpleCheck() {
+  if (block[block.length - 1].classList.contains("pointed")) checkPuzzle();
+}
+
 window.addEventListener("keydown", (event) => {
-  if (event.key === "ArrowLeft" && pointedNum % 4 !== 0) {
-    swapBlock(TO_LEFT);
-  } else if (event.key === "ArrowRight" && pointedNum % 4 !== 3) {
-    swapBlock(TO_RIGHT);
-  } else if (event.key === "ArrowUp" && pointedNum > 3) {
-    swapBlock(TO_UP);
-  } else if (event.key === "ArrowDown" && pointedNum < 12) {
-    swapBlock(TO_DOWN);
+  if (event.key === "ArrowLeft" && pointedNum % size !== 0) {
+    swapBlock(-1);
+  } else if (event.key === "ArrowRight" && pointedNum % size !== size - 1) {
+    swapBlock(1);
+  } else if (event.key === "ArrowUp" && pointedNum > size - 1) {
+    swapBlock(-size);
+  } else if (event.key === "ArrowDown" && pointedNum < size * (size - 1)) {
+    swapBlock(size);
   }
 
   // 퍼즐 맞췄는지 확인
@@ -86,4 +108,14 @@ window.addEventListener("keydown", (event) => {
   }
 });
 
+shuffleButton.addEventListener("click", () => {
+  shuffleBlock();
+  simpleCheck();
+});
+
+makePuzzle(4);
+const block = document.querySelectorAll(".block");
+
 shuffleBlock(); // 처음 셔플
+
+simpleCheck(); // 혹시 모를 맨 처음 체크
